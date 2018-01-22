b30bba6de7baf9d81c734c979d32d975a40f7204
evertondenis <evertondenis@gmail.com>
2018-01-22 21:16:15 -0200

    query all posts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { graphql, compose } from 'react-apollo'
+import gql from 'graphql-tag';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    const data = nextProps.data
+    if(data) {
+      console.log(data);
+      this.setState({
+        posts: data.allPosts
+      }, () => {
+        console.log(this.state)
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Readable App</h1>
         </header>
         <p className="App-intro">
@@ -18,4 +37,20 @@ class App extends Component {
   }
 }
 
-export default App;
+const Query = gql`{
+  posts {
+    id
+    timestamp
+    title
+    body
+    author
+    category
+    voteScore
+    deleted
+    commentCount
+  }
+}`
+
+export default compose(
+  graphql(Query)
+)(App);
