7bb47403bd0e83abc5e7cf2e50d3d6ec97dcbae9
Everton Denis <evertondenis@gmail.com>
2018-02-21 11:17:30 -0300

    change router

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import Home from './container/Home'
 import Post from './container/Post'
 
 const Routes = () => (
-  <Router>
+  <BrowserRouter>
     <div>
       <main>
         <Route exact path='/' component={Home} />
         <Route path='/post/:id' component={Post} />
       </main>
     </div>
-  </Router>
+  </BrowserRouter>
 )
 
 export default Routes
