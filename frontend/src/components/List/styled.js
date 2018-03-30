8d9ca0eeca0d2e78187a6d01e6bf52fa576be2bf
Everton Denis <evertondenis@gmail.com>
2018-03-30 10:48:43 -0300

    change box-shadow style

--- frontend/src/components/List/styled.js
+++ frontend/src/components/List/styled.js
@@ -5,13 +5,14 @@ export const ItemStyled = styled.div`
   padding: 10px;
   background-color: white;
   border-radius: 5px;
-  box-shadow: 5px 5px 25px 0 rgba(46,61,73,.2);
   border-radius: .375rem;
   -webkit-transition: all .3s ease;
   transition: all .3s ease;
+  -webkit-box-shadow: 2px 4px 8px 0 rgba(46,61,73,.2);
+  box-shadow: 2px 4px 8px 0 rgba(46,61,73,.2);
 
   :hover {
-    -webkit-box-shadow: 2px 4px 8px 0 rgba(46,61,73,.2);
-    box-shadow: 2px 4px 8px 0 rgba(46,61,73,.2);
+    -webkit-box-shadow: 5px 5px 25px 0 rgba(46,61,73,.2);
+    box-shadow: 5px 5px 25px 0 rgba(46,61,73,.2);
   }
 `
