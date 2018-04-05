2ee4b601d97079ee43569ffad8c916c6b64eb4dd
Everton Denis <evertondenis@gmail.com>
2018-04-05 09:36:57 -0300

    change theme

--- frontend/src/container/Main/index.jsx
+++ frontend/src/container/Main/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import { Header } from './styled'
 import '../../core/assets/style/main.css'
 
 const Main = ({ children }) => (
   <div className="container">
-    <Header className="App-header">
-      <h1 className="App-title">...</h1>
-    </Header>
     { children }
   </div>
 )
